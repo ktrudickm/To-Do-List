8b0badf683c005c26e2afc856cbefbdaa3e1b94a
Kendall <ktrudickm@gmail.com>
2023-10-06 14:19:18 -0700

    Completed backend: database routes working for adding to-do, deleting to-do, and updating to-do

--- backend/routes/ToDoRoutes.js
+++ backend/routes/ToDoRoutes.js
@@ -1,9 +1,12 @@
-const {Router} = require('express')
+const { Router } = require('express');
+const { getToDo, saveToDo, updateToDo, deleteToDo } = require('../controllers/ToDoController');
+
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.json({message: "Hi There!"})
-})
+router.get('/', getToDo)
+router.post('/save', saveToDo)
+router.post('/update', updateToDo)
+router.post('/delete', deleteToDo)
 
 module.exports = router;
